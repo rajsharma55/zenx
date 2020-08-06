edb52bac9ffee307e2efa50883bf66de90cf03ca
CatzBurry <myanonymoussad@gmail.com>
2020-08-07 00:30:01 +0700

    msm8953-common: XiaomiParts: Add Spectrum Profile
    Signed-off-by: CatzBurry <myanonymoussad@gmail.com>
    Signed-off-by: raj sharma <rajmwr5785@gmail.com>

--- XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
+++ XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
@@ -27,6 +27,17 @@ public class DeviceSettings extends PreferenceFragment implements
 
     private static final String AMBIENT_DISPLAY = "ambient_display_gestures";
 
+<<<<<<< HEAD
+=======
+    private static final String PREF_SPECTRUM = "spectrum";
+    private static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
+
+    private static final String PREF_ENABLE_DIRAC = "dirac_enabled";
+    private static final String PREF_HEADSET = "dirac_headset_pref";
+    private static final String PREF_PRESET = "dirac_preset_pref";
+
+    private SecureSettingListPreference mSPECTRUM;
+>>>>>>> 5d8c5b5... msm8953-common: XiaomiParts: Add Spectrum Profile
 
     //public static final String PREF_TORCH_BRIGHTNESS = "torch_brightness";
     //private static final String TORCH_1_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
@@ -46,6 +57,38 @@ public class DeviceSettings extends PreferenceFragment implements
         //        FileUtils.fileWritable(TORCH_2_BRIGHTNESS_PATH));
         //torch_brightness.setOnPreferenceChangeListener(this);
 
+<<<<<<< HEAD
+=======
+        boolean enhancerEnabled;
+        try {
+            enhancerEnabled = DiracService.sDiracUtils.isDiracEnabled();
+        } catch (java.lang.NullPointerException e) {
+            getContext().startService(new Intent(getContext(), DiracService.class));
+            try {
+                enhancerEnabled = DiracService.sDiracUtils.isDiracEnabled();
+            } catch (NullPointerException ne) {
+                // Avoid crash
+                ne.printStackTrace();
+                enhancerEnabled = false;
+            }
+        }
+
+        mSPECTRUM = (SecureSettingListPreference) findPreference(PREF_SPECTRUM);
+        mSPECTRUM.setValue(FileUtils.getStringProp(SPECTRUM_SYSTEM_PROPERTY, "0"));
+        mSPECTRUM.setSummary(mSPECTRUM.getEntry());
+        mSPECTRUM.setOnPreferenceChangeListener(this);
+
+        SecureSettingSwitchPreference enableDirac = (SecureSettingSwitchPreference) findPreference(PREF_ENABLE_DIRAC);
+        enableDirac.setOnPreferenceChangeListener(this);
+        enableDirac.setChecked(enhancerEnabled);
+
+        SecureSettingListPreference headsetType = (SecureSettingListPreference) findPreference(PREF_HEADSET);
+        headsetType.setOnPreferenceChangeListener(this);
+
+        SecureSettingListPreference preset = (SecureSettingListPreference) findPreference(PREF_PRESET);
+        preset.setOnPreferenceChangeListener(this);
+
+>>>>>>> 5d8c5b5... msm8953-common: XiaomiParts: Add Spectrum Profile
         PreferenceCategory displayCategory = (PreferenceCategory) findPreference(CATEGORY_DISPLAY);
 
         SwitchPreference usbfastCharger = (SwitchPreference) findPreference(PREF_USB_FASTCHARGE);
@@ -77,7 +120,44 @@ public class DeviceSettings extends PreferenceFragment implements
             //    FileUtils.setValue(TORCH_2_BRIGHTNESS_PATH, (int) value);
             //    break;
 
+<<<<<<< HEAD
             
+=======
+           case PREF_SPECTRUM:
+                mSPECTRUM.setValue((String) value);
+                mSPECTRUM.setSummary(mSPECTRUM.getEntry());
+                FileUtils.setStringProp(SPECTRUM_SYSTEM_PROPERTY, (String) value);
+                break;
+
+
+            case PREF_ENABLE_DIRAC:
+                try {
+                    DiracService.sDiracUtils.setEnabled((boolean) value);
+                } catch (java.lang.NullPointerException e) {
+                    getContext().startService(new Intent(getContext(), DiracService.class));
+                    DiracService.sDiracUtils.setEnabled((boolean) value);
+                }
+                break;
+
+            case PREF_HEADSET:
+                try {
+                    DiracService.sDiracUtils.setHeadsetType(Integer.parseInt(value.toString()));
+                } catch (java.lang.NullPointerException e) {
+                    getContext().startService(new Intent(getContext(), DiracService.class));
+                    DiracService.sDiracUtils.setHeadsetType(Integer.parseInt(value.toString()));
+                }
+                break;
+
+            case PREF_PRESET:
+                try {
+                    DiracService.sDiracUtils.setLevel(String.valueOf(value));
+                } catch (java.lang.NullPointerException e) {
+                    getContext().startService(new Intent(getContext(), DiracService.class));
+                    DiracService.sDiracUtils.setLevel(String.valueOf(value));
+                }
+                break;
+
+>>>>>>> 5d8c5b5... msm8953-common: XiaomiParts: Add Spectrum Profile
             case PREF_USB_FASTCHARGE:
                 FileUtils.setValue(USB_FASTCHARGE_PATH, (boolean) value);
                 break;
