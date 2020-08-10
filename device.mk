d72fadb9f886367440a57781b67292388fe03869
raj sharma <rajmwr5785@gmail.com>
2020-08-11 00:10:50 +0530

    Rolex: Add Moto Dolby
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>
    Signed-off-by: raj sharma <rajmwr5785@gmail.com>

--- device.mk
+++ device.mk
@@ -243,6 +243,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(DEVICE_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(DEVICE_PATH)/configs/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
+    $(DEVICE_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(DEVICE_PATH)/configs/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 
 # Net
